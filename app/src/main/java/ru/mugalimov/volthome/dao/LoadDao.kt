2ee25bbc2d45905a58fa0538ffd01adc3fe77e0c
reenat <reenat056@yandex.ru>
2025-03-25 21:27:55 +0300

    Fix loads

--- app/src/main/java/ru/mugalimov/volthome/dao/LoadDao.kt
+++ app/src/main/java/ru/mugalimov/volthome/dao/LoadDao.kt
@@ -1,12 +1,15 @@
 package ru.mugalimov.volthome.dao
 
 import androidx.room.Dao
+import androidx.room.Embedded
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import kotlinx.coroutines.flow.Flow
 import ru.mugalimov.volthome.entity.LoadEntity
 import ru.mugalimov.volthome.model.Load
+import ru.mugalimov.volthome.model.Room
 
 @Dao
 interface LoadDao {
@@ -16,4 +19,16 @@ interface LoadDao {
 
     @Insert(onConflict = OnConflictStrategy.ABORT)
     suspend fun addLoad(loadEntity: LoadEntity)
+
+    @Query("SELECT * FROM loads WHERE room_id = :roomId")
+    fun getLoadForRoom(roomId: Int): Flow<Load>
+
+    @Transaction
+    @Query("SELECT * FROM rooms")
+    fun getRoomsWithLoads(): Flow<List<RoomWithLoad>>
 }
+
+data class RoomWithLoad(
+    @Embedded val room: Room,
+    @Embedded val load: Load
+)
