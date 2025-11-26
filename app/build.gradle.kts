4969c92b61ebd204f0c6032ba11cda827b10358b
reenat <reenat056@yandex.ru>
2025-11-26 21:30:06 +0300

    Коммит перед реализацией этапа 6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         minSdk = 24
         //noinspection EditedTargetSdkVersion
         targetSdk = 35
-        versionCode = 13
-        versionName = "2.2.1"
+        versionCode = 14
+        versionName = "2.2.2"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
